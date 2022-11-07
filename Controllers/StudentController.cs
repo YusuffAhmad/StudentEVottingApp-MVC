9960ccbe52a9a32721b0200fc6e848a5e2d5b38b
YusuffAhmad <yusuffahmad2005@gmail.com>
2022-11-07 15:17:35 +0100

    first commit

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using STUDENTEVOTINGAPP.Interfaces.Repositories;
+
+namespace STUDENTEVOTINGAPP.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IStudentService _studentservice;
+
+        public StudentController(IStudentService studentservice)
+        {
+            _studentservice = studentservice;
+        }
+        //[Authorize(Roles = "Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult Create()
+        {
+            var isSuccessful = _studentservice.RegisterStudents();
+            if (isSuccessful.Status == false)
+            {
+                ViewBag.Message = isSuccessful.Message;
+                return View();
+            }
+            return RedirectToAction("Index", "Admin");
+        }
+        public IActionResult Get()
+        {
+            var students = _studentservice.GetStudents();
+            if (students == null)
+            {
+                return NotFound("No Student Registered Yet");
+            }
+            return View(students.Data);
+        }
+    }
+}
